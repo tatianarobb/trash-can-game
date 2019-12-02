8315f565fb35da0070a075fff47022086aef6548
OscarMoss21 <56734079+OscarMoss21@users.noreply.github.com>
2019-12-02 14:41:19 -0800

    Built trashcan w/ the dude in the game

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -1,37 +1,50 @@
 //create an empty array called balls
-let balls = [];
+//let balls = [];
 
 //create a variable to hold your avatar
 let me;
 
-
 function setup() {
   createCanvas(500, 400);
 
+
   //make one avatar called me
   me = new Avatar(width/2, 300, 3);
+  can = new trashcan(450, 300);
 
 }
 
-function draw(){
-	background(220);
+ function draw(){
+ 	background(220);
+  // trashcan(450, 300);
 
-  me.drawMe();
-  me.moveMe();
+   me.drawMe();
+   me.moveMe();
+   can.drawtrashcan();
 
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -random(2,5));
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
-    }
+ }
 
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
-	  }
+class trashcan {
+  constructor(x, y){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+   }
 
+drawtrashcan(){
+//x=150 y=150
+  rect(this.x, this.y, 40, 60);
+  rect(this.x-5, this.y, 50, 10);
+  fill("red")
+  line(this.x+5, this.y+60, this.x+5, this.y+10);
+  fill("black")
+  line(this.x+10, this.y+60, this.x+10, this.y+10);
+  line(this.x+15, this.y+60, this.x+15, this.y+10);
+  line(this.x+20, this.y+60, this.x+20, this.y+10);
+  line(this.x+25, this.y+60, this.x+25, this.y+10);
+  line(this.x+30, this.y+60, this.x+30, this.y+10);
+  line(this.x+35, this.y+60, this.x+35, this.y+10);
+
+   }
 }
 
 //avatar class
@@ -69,50 +82,5 @@ class Avatar {
   die(){
 
   }
-
-}
-
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
-		this.x = x;
-    this.y = y;
-    this.speed = speed;
-	}
-
-	// draw a ball on the screen at x,y
-	drawBall(){
-    	stroke(0);
-      strokeWeight(1);
-    	 var RGBColor = (Math.round, Math.random, 255)
-		  rect(this.x,this.y,25,25);
-	}
-
-	//update the location of the ball, so it moves across the screen
-	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
-	}
-
-	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-100 && this.x <= me.x+100 && this.y > me.y-100 && this.y < me.y+100){
-      			this.speed = -this.speed;
-
-    function draw() {
-        background(220);
-        rect(50, 50, 40, 60);
-        rect(45, 50, 50, 10);
-        arc(60, 30, 10, 10, 5, 90, 180);
-        line(55, 60, 55, 110);
-        line(60, 60, 60, 110);
-        line(65, 60, 65, 110);
-        line(70, 60, 70, 110);
-        line(75, 60, 75, 110);
-    		}
-  	}
 
 }
